a09eee0a07b7681592007bb5f4596420f2825942
kuj6022 <kuj6022@someplace.com>
2024-11-01 12:28:30 +0900

    initial commit

---
+++ three-js-boilerplate/three-js-webpack/src/ex-03.js
@@ -0,0 +1,32 @@
+import * as THREE from 'three'
+
+import {
+    WEBGL
+}   from './webgl'
+
+if (WEBGL. isWebGLAvailable()) {
+
+    // 장면
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x004fff)
+
+    // 카메라
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    // 캔버스
+    const canvas = document.querySelector('#ex-03');
+
+
+
+    // 렌더러
+    const renderer = new THREE.WebGLRenderer({canvas});
+    renderer.setSize(window.innerWidth, window.innerHeight)
+
+    document.body.appendChild(renderer.domElement);
+
+    renderer.render(scene, camera)
+
+} else {
+    var warning = WEBGL.getWebGLErrorMessage();
+    document.body.appendChild(warning);
+}
